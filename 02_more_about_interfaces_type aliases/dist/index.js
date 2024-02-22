fff8ea51a60cdfa97bfdd199bebefeda21d5125b
Mark Slorach <hello@markslorach.com>
2024-02-22 11:18:26 +0000

    update

---
+++ 02_more_about_interfaces_type aliases/dist/index.js
@@ -0,0 +1,2 @@
+"use strict";
+const something = { quantity: 50 };
---
+++ 02_more_about_interfaces_type aliases/src/index.ts
@@ -0,0 +1,10 @@
+// REUSABLE INTERFACES
+interface hasQuantity {
+    quantity: number
+}
+
+const something: hasQuantity = {quantity: 50};
+
+function printQuantity(item: hasQuantity): void {
+
+}
