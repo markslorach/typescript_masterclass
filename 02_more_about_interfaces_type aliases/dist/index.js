16b1649f653929531979ecaee74c6b54f5b1e2bc
Mark Slorach <hello@markslorach.com>
2024-02-24 13:28:30 +0000

    update

--- 02_more_about_interfaces_type aliases/dist/index.js
+++ 02_more_about_interfaces_type aliases/dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
 const something = { quantity: 50 };
+function printQuantity(item) {
+    console.log(`The gauntity if the item is ${item.quantity}`);
+}
+const fruit = {
+    name: "Mango",
+    quantity: 50,
+};
+const vehicle = {
+    type: "Car",
+    quantity: 3,
+};
+const person = {
+    name: "Mark",
+    age: 32,
+};
+printQuantity(fruit);
+printQuantity(vehicle);
+// wont work as person object does not have a quantify property outlined by the interface
+printQuantity(person);
--- 02_more_about_interfaces_type aliases/src/index.ts
+++ 02_more_about_interfaces_type aliases/src/index.ts
@@ -1,10 +1,35 @@
 // REUSABLE INTERFACES
 interface hasQuantity {
-    quantity: number
+  quantity: number;
 }
 
-const something: hasQuantity = {quantity: 50};
+const something: hasQuantity = { quantity: 50 };
 
 function printQuantity(item: hasQuantity): void {
-
+  console.log(`The gauntity if the item is ${item.quantity}`);
 }
+
+const fruit = {
+  name: "Mango",
+  quantity: 50,
+};
+
+const vehicle = {
+  type: "Car",
+  quantity: 3,
+};
+
+const person = {
+  name: "Mark",
+  age: 32,
+};
+
+printQuantity(fruit);
+printQuantity(vehicle);
+
+// wont work as person object does not have a quantify property outlined by the interface
+// printQuantity(person);
+
+// FUNCTION SIGNATURES
+// A function signature defines the input parameters and their types and also the expected return type for that function.
+
